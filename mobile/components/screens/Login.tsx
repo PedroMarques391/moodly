1bf10f673005e52ddc1792db6dcceb895023a5e5
Pedro Marques <pedromarques391@gmail.com>
2025-11-01 00:18:52 -0300

    feat: create a form to signin and login.

---
+++ mobile/components/screens/Login.tsx
@@ -0,0 +1,56 @@
+import globals from "@/styles/globals";
+import { LoginData, loginScheme } from "@/validations/login.scheme";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { View } from "react-native";
+import Btn from "../ui/Button";
+import Input from "../ui/Input";
+
+const Login = (): React.JSX.Element => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<LoginData>({
+    resolver: zodResolver(loginScheme),
+    mode: "onSubmit",
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function handleLogin(data: LoginData) {
+    console.warn(data);
+    reset();
+  }
+
+  console.log(errors);
+  return (
+    <View style={globals.container}>
+      <Input
+        name="email"
+        placeholder="email@example.com"
+        label="E-mail"
+        mode="outlined"
+        control={control}
+        formError={errors.email?.message || ""}
+      />
+      <Input
+        name="password"
+        placeholder="*******"
+        label="Senha"
+        mode="outlined"
+        control={control}
+        formError={errors.password?.message}
+      />
+
+      <Btn mode="contained" onPress={handleSubmit(handleLogin)} icon="login">
+        Acessar
+      </Btn>
+    </View>
+  );
+};
+
+export default Login;
---
+++ mobile/components/screens/SignIn.tsx
@@ -0,0 +1,77 @@
+import globals from "@/styles/globals";
+import { SignInData, signInSchema } from "@/validations/signIn.shema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { View } from "react-native";
+import Btn from "../ui/Button";
+import Input from "../ui/Input";
+
+const SignIn = (): React.JSX.Element => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<SignInData>({
+    resolver: zodResolver(signInSchema),
+    mode: "onSubmit",
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+  function handleSignIn(data: SignInData) {
+    console.warn(data);
+    reset();
+  }
+
+  return (
+    <View style={globals.container}>
+      <Input
+        name="name"
+        placeholder="Digite seu nome"
+        label="Nome"
+        mode="outlined"
+        control={control}
+        formError={errors.name?.message}
+      />
+      <Input
+        name="email"
+        placeholder="email@example.com"
+        label="E-mail"
+        mode="outlined"
+        control={control}
+        formError={errors.email?.message}
+      />
+      <Input
+        name="password"
+        placeholder="*******"
+        label="Senha"
+        mode="outlined"
+        control={control}
+        formError={errors.password?.message}
+      />
+
+      <Input
+        name="confirmPassword"
+        placeholder="*******"
+        label="Confirmar Senha"
+        mode="outlined"
+        control={control}
+        formError={errors.confirmPassword?.message}
+      />
+
+      <Btn
+        mode="contained"
+        onPress={handleSubmit(handleSignIn)}
+        icon="account-plus"
+      >
+        Criar conta
+      </Btn>
+    </View>
+  );
+};
+
+export default SignIn;
