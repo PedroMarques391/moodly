bd02cb2c0e9a37b5650b110646f31b63dafe7266
Pedro Marques <pedromarques391@gmail.com>
2025-11-01 00:23:07 -0300

    feat: create index page.

---
+++ mobile/app/index.tsx
@@ -0,0 +1,14 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+const HomePage = (): React.JSX.Element => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Link href="/auth">
+        <Text>Go to Auth Home Screen</Text>
+      </Link>
+    </View>
+  );
+};
+
+export default HomePage;
