05b87e1a1d44a24b4f808110dbd51d6d9e1d135f
Pedro Marques <pedromarques391@gmail.com>
2025-11-01 11:03:50 -0300

    refactor: refactor auth page to add KeyboardAvoidingView and ScrollView.

--- mobile/app/(auth)/auth.tsx
+++ mobile/app/(auth)/auth.tsx
@@ -1,39 +1,54 @@
 import Login from "@/components/screens/Login";
 import SignIn from "@/components/screens/SignIn";
 import { useState } from "react";
-import { Dimensions, Image, Text, TouchableOpacity, View } from "react-native";
-import { styles } from "./auth.styles";
-
-const { height } = Dimensions.get("window");
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import styles from "./auth.styles";
 
 const AuthPage = (): React.JSX.Element => {
-  const [type, setType] = useState<"signin" | "login">("signin");
+  const [screen, setScreen] = useState<"signin" | "login">("signin");
 
   return (
-    <View style={styles.container}>
-      <View style={styles.logoContainer}>
-        <Image
-          source={require("../../assets/logo2.png")}
-          style={styles.logo}
-          resizeMode="cover"
-        />
-      </View>
-
-      <View style={styles.formContainer}>
-        {type === "signin" ? <SignIn /> : <Login />}
-      </View>
-
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <ScrollView
+          contentContainerStyle={{ flexGrow: 1 }}
+          keyboardShouldPersistTaps="handled"
+          showsVerticalScrollIndicator={false}
+        >
+          <View style={styles.container}>
+            <View style={styles.logoContainer}>
+              <Image
+                source={require("../../assets/logo3.png")}
+                style={{ width: "100%", height: "100%" }}
+                resizeMode="contain"
+              />
+            </View>
+            {screen === "signin" ? <SignIn /> : <Login />}
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
       <TouchableOpacity
         style={styles.switchButton}
-        onPress={() => setType(type === "login" ? "signin" : "login")}
+        onPress={() => setScreen(screen === "login" ? "signin" : "login")}
       >
         <Text style={styles.switchText}>
-          {type === "login"
+          {screen === "login"
             ? "Não tem conta? Crie em 1 minuto!"
             : "Já tem conta? Fazer login"}
         </Text>
       </TouchableOpacity>
-    </View>
+    </>
   );
 };
 
