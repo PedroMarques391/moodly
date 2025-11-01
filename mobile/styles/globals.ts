dd157b8759db9c2ef13fbf318bdf99f5de6d8f39
Pedro Marques <pedromarques391@gmail.com>
2025-11-01 00:20:13 -0300

    feat: add global theme.

---
+++ mobile/styles/globals.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+const globals = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
+  },
+});
+
+export default globals;
