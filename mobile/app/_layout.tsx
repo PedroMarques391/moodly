11b5dd0a0b8a914eb55682ad41f775671b850882
Pedro Marques <pedromarques391@gmail.com>
2025-11-01 00:20:59 -0300

    feat: add PaperProvider from react-native-paper in _layout root.

---
+++ mobile/app/_layout.tsx
@@ -0,0 +1,36 @@
+import { theme as moodlyTheme } from "@/theme/theme";
+import { Slot } from "expo-router";
+import React from "react";
+import { View } from "react-native";
+import { DefaultTheme, PaperProvider } from "react-native-paper";
+import "react-native-reanimated";
+
+const paperTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: moodlyTheme.colors.primary,
+    secondary: moodlyTheme.colors.secondary,
+    background: moodlyTheme.colors.background,
+    surface: moodlyTheme.colors.surface,
+    text: moodlyTheme.colors.textPrimary,
+    placeholder: moodlyTheme.colors.textSecondary,
+    error: moodlyTheme.colors.error,
+  },
+  roundness: moodlyTheme.radius.md,
+};
+
+export default function RootLayout() {
+  return (
+    <PaperProvider theme={paperTheme}>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: paperTheme.colors.background,
+        }}
+      >
+        <Slot />
+      </View>
+    </PaperProvider>
+  );
+}
