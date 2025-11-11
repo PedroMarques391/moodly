b81f06b6880b4c315117a4aa51ce8c5b538fe2e2
Pedro Marques <pedromarques391@gmail.com>
2025-11-11 16:57:45 -0300

    feat: create user.repository where all the basic operations for a user are performed.

---
+++ apps/backend/src/repository/UserRepository.ts
@@ -0,0 +1,30 @@
+import { CreateUser, LoginUser, User, UserRepositoryModel } from "@moodly/core";
+import { prisma } from "../database/initialize";
+
+export class UserRepository implements UserRepositoryModel {
+  async findByEmail(email: string) {
+    return prisma.user.findUnique({ where: { email } });
+  }
+  async createUser({ name, email, password }: CreateUser) {
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+        image: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+        password,
+      },
+    });
+  }
+
+  async login({ email, password }: LoginUser): Promise<User | null> {
+    throw new Error("Method not implemented.");
+  }
+
+  async findById(id: string): Promise<User | null> {
+    throw new Error("Method not implemented.");
+  }
+
+  async update(id: string, user: Partial<User>): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+}
