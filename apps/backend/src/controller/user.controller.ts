494a1059abc1e58b8d03d0f42b122da3074cf46a
Pedro Marques <pedromarques391@gmail.com>
2025-11-12 00:56:43 -0300

    feat(backend/src/controller/user.controller.ts): add jwt sign after createUser.

--- apps/backend/src/controller/user.controller.ts
+++ apps/backend/src/controller/user.controller.ts
@@ -1,5 +1,6 @@
 import { CreateUser } from "@moodly/core";
 import { FastifyInstance } from "fastify";
+import { authMiddleware } from "../middlewares/auth.middleware";
 import userService from "../services/user.service";
 
 export default function userController(fastify: FastifyInstance) {
@@ -10,9 +11,16 @@ export default function userController(fastify: FastifyInstance) {
 
     try {
       await userService.createUser({ name, email, password });
-      reply.send({ message: "user created" }).code(201);
+      const payload = { name, email };
+      const token = fastify.jwt.sign(payload);
+      reply.send({ message: "user created", token }).code(201);
     } catch (error) {
       reply.send(error).code(400);
     }
+  });
+
+  fastify.get("/", { preHandler: authMiddleware }, async (req, reply) => {
+    const payload = req.user;
+    reply.send(payload);
   });
 }
