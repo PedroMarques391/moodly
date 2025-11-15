e22ed55225d3804eebdfcd650fcf89e7feafc02b
Pedro Marques <pedromarques391@gmail.com>
2025-11-15 10:29:06 -0300

    feat: Profile and Modal component in settings screen.

--- apps/mobile/app/(home)/settings.tsx
+++ apps/mobile/app/(home)/settings.tsx
@@ -1,10 +1,19 @@
+import Modal from "@/components/ui/Modal";
 import Profile from "@/components/ui/Profile";
 import { useAuthStore } from "@/store/auth.store";
+import { useState } from "react";
 import { View } from "react-native";
 
 export default function Settings() {
   const { user } = useAuthStore();
+  const [showModal, setShowModal] = useState<boolean>(false);
+
   if (!user) return null;
+
+  function handleShowModal() {
+    setShowModal((prev) => !prev);
+  }
+
   return (
     <View>
       <Profile
@@ -12,6 +21,14 @@ export default function Settings() {
         email={user.email}
         image={user.image}
         createdAt={user.createdAt}
+        handleShowModal={handleShowModal}
+      />
+      <Modal
+        visible={showModal}
+        onDismiss={handleShowModal}
+        name={user.name}
+        email={user.email}
+        image={user.image}
       />
     </View>
   );
