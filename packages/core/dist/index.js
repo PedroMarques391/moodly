b9a117aa644fc8f39a4773a3bbe5e0cc477ebbee
Pedro Marques <pedromarques391@gmail.com>
2025-11-16 17:14:46 -0300

    feat: finish settings page.

--- packages/core/dist/index.js
+++ packages/core/dist/index.js
@@ -1,1 +1,18 @@
-"use strict";var p=Object.defineProperty;var f=Object.getOwnPropertyDescriptor;var x=Object.getOwnPropertyNames;var s=Object.prototype.hasOwnProperty;var U=(o,r,m,t)=>{if(r&&typeof r=="object"||typeof r=="function")for(let e of x(r))!s.call(o,e)&&e!==m&&p(o,e,{get:()=>r[e],enumerable:!(t=f(r,e))||t.enumerable});return o};var a=o=>U(p({},"__esModule",{value:!0}),o);var i={};module.exports=a(i);
+"use strict";
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// src/index.ts
+var index_exports = {};
+module.exports = __toCommonJS(index_exports);
