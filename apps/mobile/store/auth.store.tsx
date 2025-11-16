854fdbd1f3f161407606113e28cd39641c4347a2
Pedro Marques <pedromarques391@gmail.com>
2025-11-15 22:56:12 -0300

    feat: ajust order.

--- apps/mobile/store/auth.store.tsx
+++ apps/mobile/store/auth.store.tsx
@@ -3,9 +3,9 @@ import { create } from "zustand";
 
 export const useAuthStore = create<IAuthStore>((set) => ({
   user: null,
-  setUser: async (user) => set(() => ({ user })),
   isLoading: false,
   error: null,
+  setUser: async (user) => set({ user }),
   setError: (error) => set({ error }),
   setIsLoading: (loading) => set({ isLoading: loading }),
 }));
