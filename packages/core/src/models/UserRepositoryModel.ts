b9a17394f4a26f48780619cb0672e8d838125f28
Pedro Marques <pedromarques391@gmail.com>
2025-11-17 11:04:11 -0300

    feat: add user DTOs in UserRepositoryModel.

--- packages/core/src/models/UserRepositoryModel.ts
+++ packages/core/src/models/UserRepositoryModel.ts
@@ -1,8 +1,9 @@
-import { User, CreateUser, Payload } from "./user";
+import { createUserDTO } from "../dto";
+import { Payload, User } from "./user";
 
 export default interface UserRepositoryModel {
   findByEmail(email: string): Promise<User | null>;
-  createUser(user: CreateUser): Promise<Payload>;
+  createUser(user: createUserDTO): Promise<Payload>;
   findById(id: string): Promise<User | null>;
   update(id: string, user: Partial<User>): Promise<void>;
 }
