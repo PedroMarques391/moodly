ca35916d2be99bf4c2d4b938899b62cdf28e794c
Pedro Marques <pedromarques391@gmail.com>
2025-11-17 11:03:19 -0300

    feat: create mood repository Model.

---
+++ packages/core/src/models/MoodRepositoryModel.ts
@@ -0,0 +1,18 @@
+import { UpdateMoodDTO } from "../../dist";
+import { CreateMoodDTO } from "../dto";
+import { Mood } from "./mood/mood";
+
+export default interface MoodRepositoryModel {
+  createMood(userId: string, data: CreateMoodDTO): Promise<void>;
+  deleteMood(id: string): Promise<void>;
+  updateMood(id: string, data: UpdateMoodDTO): Promise<void>;
+  getMoodById(id: string): Promise<Mood | null>;
+  getMoods(userId: string): Promise<Mood[] | []>;
+  getMoodsByDateRange(
+    userId: string,
+    initalDate: Date,
+    finalDate: Date
+  ): Promise<Mood[] | []>;
+
+  checkMoodExistsByDate(userId: string, date: Date): Promise<boolean>;
+}
