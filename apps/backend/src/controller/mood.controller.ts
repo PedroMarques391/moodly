35b22fff4ab1588146f45fe74e0af621d8390a2c
Pedro Marques <pedromarques391@gmail.com>
2025-11-17 11:09:45 -0300

    feat: create mood routes. Delete, Update, Get, Post.

---
+++ apps/backend/src/controller/mood.controller.ts
@@ -0,0 +1,53 @@
+import { CreateMoodDTO } from "@moodly/core";
+import { FastifyInstance } from "fastify";
+import moodService from "../services/mood.service";
+
+export default function moodController(fastify: FastifyInstance) {
+  fastify.post<{ Body: CreateMoodDTO; Params: { id: string } }>(
+    "/:id",
+    async (req, reply) => {
+      const { id } = req.params;
+      const data = req.body;
+      try {
+        await moodService.createMood(id, data);
+        reply.send().code(201);
+      } catch (error) {
+        reply.send(error).code(400);
+      }
+    }
+  );
+
+  fastify.get<{ Params: { id: string } }>("/:id", async (req, reply) => {
+    const { id } = req.params;
+    try {
+      const moods = await moodService.getMoods(id);
+      reply.send(moods).code(200);
+    } catch (error) {
+      reply.send(error).code(400);
+    }
+  });
+
+  fastify.delete<{ Params: { id: string } }>("/:id", async (req, reply) => {
+    const { id } = req.params;
+    try {
+      await moodService.deleteMood(id);
+      reply.send().code(204);
+    } catch (error) {
+      reply.send(error).code(400);
+    }
+  });
+
+  fastify.put<{ Body: CreateMoodDTO; Params: { id: string } }>(
+    "/:id",
+    async (req, reply) => {
+      const { id } = req.params;
+      const data = req.body;
+      try {
+        await moodService.updateMood(id, data);
+        reply.send({ message: "MOOD_UPDATED" }).code(204);
+      } catch (error) {
+        reply.send(error).code(400);
+      }
+    }
+  );
+}
