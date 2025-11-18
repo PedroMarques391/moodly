4be2388b483cf6f459c90a6623bf94b41ed18082
Pedro Marques <pedromarques391@gmail.com>
2025-11-18 19:02:58 -0300

    feat: add Swagger to docs.

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,4 +1,6 @@
 import fastifyJwt from "@fastify/jwt";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 import fastify, { FastifyInstance } from "fastify";
 import moodController from "./controller/mood.controller";
 import userController from "./controller/user.controller";
@@ -13,6 +15,16 @@ server.register(fastifyJwt, {
   secret: process.env.JWT_SECRET,
 });
 
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "@moodly-api",
+      description: "Documentação da API do aplicativo moodly.",
+      version: "1.0.0",
+    },
+  },
+});
+
 server.register(errorHandlerPlugin);
 
 server.register(
@@ -25,6 +37,7 @@ server.register(
   },
   { prefix: "/api/v1" }
 );
+server.register(fastifySwaggerUi, { routePrefix: "/docs" });
 server.listen({ port: port, host: "0.0.0.0" }, (err, _) => {
   if (err) {
     console.error(err);
