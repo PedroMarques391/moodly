e503028f0e0dd7b9bb7e8c9fce63224a2524c926
Pedro Marques <pedromarques391@gmail.com>
2025-12-27 18:20:33 -0300

    feat: add loading to profile avatar.

--- apps/mobile/components/ui/Profile.tsx
+++ apps/mobile/components/ui/Profile.tsx
@@ -3,7 +3,8 @@ import { theme } from "@/theme/theme";
 import { formatDate } from "@/utils/formatDate";
 import { Feather } from "@expo/vector-icons";
 import { User } from "@moodly/core";
-import { Text, TouchableOpacity, View } from "react-native";
+import { useState } from "react";
+import { ActivityIndicator, Text, TouchableOpacity, View } from "react-native";
 import { Avatar } from "react-native-paper";
 
 type ProfileProps = Pick<User, "name" | "email" | "image" | "createdAt"> & {
@@ -19,9 +20,38 @@ export default function Profile({
 }: ProfileProps): React.JSX.Element {
   const creationDate = formatDate(createdAt);
 
+  const [isImageLoading, setIsImageLoading] = useState(true);
+
   return (
     <View style={profile.card}>
-      <Avatar.Image size={64} source={{ uri: image }} />
+      <View style={{ position: "relative", height: 64, width: 64 }}>
+        <Avatar.Image
+          size={64}
+          source={{ uri: image }}
+          onLoadEnd={() => setIsImageLoading(false)}
+          onError={() => setIsImageLoading(false)}
+        />
+
+        {isImageLoading && (
+          <View
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: 64,
+              height: 64,
+              borderRadius: 32,
+              backgroundColor: "#ffffff",
+              justifyContent: "center",
+              alignItems: "center",
+              borderWidth: 1,
+              borderColor: theme.colors.primary,
+            }}
+          >
+            <ActivityIndicator size="small" color={theme.colors.primary} />
+          </View>
+        )}
+      </View>
       <View style={profile.textContainer}>
         <Text style={profile.name} numberOfLines={1}>
           {name}
